3e61e45f401b379ef4d7c366ccd856bea33016b6
lakshu17 <betelguesebusiness@gmail.com>
2025-09-15 17:13:36 +0530

    Argon issue solved - 81

--- types/core.d.ts
+++ types/core.d.ts
@@ -1,4 +1,4 @@
-export function encryptAndStore(itemKey: any, plainText: any, passphrase: any, options?: {}): Promise<void>;
+export function encryptAndStore(itemKey: any, plainString: any, passphrase: any, options?: {}): Promise<void>;
 export function getAndDecrypt(itemKey: any, passphrase: any): Promise<ArrayBuffer | null>;
 export function removeItem(itemKey: any): Promise<void>;
 export function clearAll(): Promise<void>;
