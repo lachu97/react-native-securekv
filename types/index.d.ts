5d18d79403d4c55ad5b06f14db4160cf4e820bee
lakshu17 <betelguesebusiness@gmail.com>
2025-09-15 13:22:56 +0530

    Argon issue solved - 1

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+export default SecureKV;
+declare namespace SecureKV {
+    function setItem(key: any, value: any, passphrase: any, options?: {}): Promise<void>;
+    function getItem(key: any, passphrase: any): Promise<string | null>;
+    function removeItem(key: any): Promise<void>;
+    function clear(): Promise<void>;
+    function verify(passphrase: any): Promise<boolean>;
+    function createVerifyBlob(passphrase: any): Promise<void>;
+}
